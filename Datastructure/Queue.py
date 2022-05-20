65a4feee429cfa3bd14c0b9bd45352d3c6f3b05f
rll2641 <fair0838@gmail.com>
2022-05-20 17:37:53 +0900

    원형 큐

--- Datastructure/Queue.py
+++ Datastructure/Queue.py
@@ -1,15 +1,31 @@
 class Queue:
-    def __init__(self):
-        self.queue = []
-    def queue_size(self):
-        return len(self.queue)
+    def __init__(self, que_size):
+        self.queue = [0 for _ in range(que_size)]
+        self.que_size = que_size
+        self.front = self.rear = 0
+        
     def is_empty(self):
-        return (self.queue_size() == 0)
+        return (self.front == self.rear)
+    
+    def is_full(self):
+        return (self.front == (self.rear + 1) % self.que_size)
+    
+    def current_que_size(self):
+        return len(self.queue)
+    
     def enqueue(self, num):
-        self.queue.append(0, num)
+        if self.is_full():
+            print("queue is full")
+            return
+        else:
+            self.rear = (self.rear + 1) % self.que_size
+            self.queue[self.rear] = num
+    
     def dequeue(self):
         if self.is_empty():
             print("queue is empty")
+            return
         else:
-            return self.queue.pop(0)
-    
+            self.front = (self.front + 1) % self.que_size
+            return self.queue[self.front]
+        
