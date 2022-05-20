ecb69d0fd1e92ccbab7597ab1b4b6f4a7e6fcc0a
rll2641 <fair0838@gmail.com>
2022-05-20 17:37:47 +0900

    이중연결리스트

--- Datastructure/Double_Linked_list.py
+++ Datastructure/Double_Linked_list.py
@@ -3,15 +3,15 @@ class Node:
         self.data = data
         self.left = left
         self.right = right
-    
-class DLinked_list:
+        
+class Dlinked_list:
     def __init__(self):
         self.head = Node(None)
         self.head.left = self.head.right = self.head
-        self.node_size = 0
+        self.node_count = 0
     
-    def dlist_size(self):
-        return self.node_size
+    def list_size(self):
+        return self.node_count
     
     def insert_node(self, pre, data):
         new_node = Node(data)
@@ -19,32 +19,26 @@ class DLinked_list:
         new_node.right = pre.right
         new_node.left.right = new_node
         new_node.right.left = new_node
+        self.node_count += 1
     
-    def search(self, data):
+    def search_list(self, data):
         cur = self.head.right
-        while cur != self.head:
+        while cur is not self.head:
             if cur.data == data:
                 return cur
             cur = cur.right
         return None
     
-    def delete_node(self, removed):
-        if(removed == self.head):
-            return
+    def delete_node(self, data):
+        removed = self.search_list(data)
+        if removed is self.head: return
         removed.left.right = removed.right
         removed.right.left = removed.left
     
-    def print_dlist(self):
+    def print_list(self):
         cur = self.head.right
-        while cur != self.head:
+        while cur is not self.head:
             print(cur.data, end=" ")
             cur = cur.right
-        
-dl = DLinked_list()
-dl.insert_node(dl.head, 5)
-dl.insert_node(dl.head, 4)
-dl.insert_node(dl.head, 3)
-dl.insert_node(dl.head, 2)  
-dl.insert_node(dl.head, 1)
-dl.delete_node(dl.search(3))
-dl.print_dlist()
+            
+    
