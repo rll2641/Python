eef28156bb55e52ae7a663802ff9d02a09782cf8
rll2641 <fair0838@gmail.com>
2022-05-20 17:38:08 +0900

    스택

--- Datastructure/Stack.py
+++ Datastructure/Stack.py
@@ -1,12 +1,16 @@
 class Stack:
     def __init__(self):
         self.stack = []
+        
     def stack_size(self):
         return len(self.stack)
+    
     def is_empty(self):
         return (self.stack_size() == 0)
+    
     def push(self, num):
         self.stack.append(num)
+        
     def pop(self):
         if self.is_empty():
             print("stack is empty")
