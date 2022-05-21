37354f0b9d82a458a179a96021db5a604b94df77
rll2641 <fair0838@gmail.com>
2022-05-21 22:12:21 +0900

    # 1021

---
+++ Algorithm/BAEKJOON/Que/회전하는 큐.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+n, m = map(int, input().split())
+loc = list(map(int, input().split()))
+d = deque([i for i in range(1, n+1)])
+
+cnt = 0
+for i in loc:
+    while True:
+        if d[0] == i:
+            d.popleft()
+            break
+        else:
+            if d.index(i) < len(d)/2:
+                while d[0] != i:
+                    d.append(d.popleft())
+                    cnt += 1
+            else:
+                while d[0] != i:
+                    d.appendleft(d.pop())
+                    cnt += 1
+print(cnt)
