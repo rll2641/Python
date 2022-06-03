ea118f53c2a61735393c1d38f235ba4110f43af0
rll2641 <fair0838@gmail.com>
2022-06-03 18:41:11 +0900

    tkinter을 이용한 지하철 최단경로

--- Python_Project/Subway_Station/subway_tkinter.py
+++ Python_Project/Subway_Station/subway_tkinter.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import tkinter
 import subway_bfs as sb
+import subway_dijkstra as dijk
 
 subway = Tk()
 subway.title("지하철 최단경로")
@@ -15,7 +16,6 @@ input_start.place(x=630, y=30)
 def receive_start():
     st = input_start.get()
     
-
 # 출발열 선택 버튼
 btn_start = Button(subway, text="출발역 선택")
 btn_start.config(width=10, height=2)
@@ -34,16 +34,31 @@ btn_end.config(width=10, height=2)
 btn_end.config(command=receive_end)
 btn_end.place(x=660, y=140)
 
-label2 = Label(subway)
-label2.place(x=520, y=300)
+# 최단 경로 출력
+label_bfs = Label(subway)
+label_bfs.place(x=520, y=300)
 def bfs_start():
     result = sb.bfs(input_start.get(), input_end.get())
-    label2.configure(text= "경로:" + "->".join(result), wraplength=300)
-
+    label_bfs.configure(text="경로:" + "->".join(result), wraplength=300)
+    
 # 최단 경로 시작
-btn3 = Button(subway, text="최단 경로")
-btn3.config(width=10, height=2)
-btn3.config(command=bfs_start)
-btn3.place(x=660, y=200)
+btn_bfs = Button(subway, text="최단 경로")
+btn_bfs.config(width=10, height=2)
+btn_bfs.config(command=bfs_start)
+btn_bfs.place(x=680, y=200)
+
+# 최단 거리 출력
+label_dijk = Label(subway)
+label_dijk.place(x=520, y=400)
+def dijk_start():
+    result = dijk.dijkstra(input_start.get())
+    label_dijk.configure(text="거리: " + str(result[input_end.get()]) + "km")
+    
+# 최단 거리
+btn_dijk = Button(subway, text="최단 거리")
+btn_dijk.config(width=10, height=2)
+btn_dijk.config(command=dijk_start)
+btn_dijk.place(x=600, y=200)
+
 
 subway.mainloop()
