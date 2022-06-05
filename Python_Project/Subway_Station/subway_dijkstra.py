f5ee3a1e36cc76aca24bd7dfd0c7eb5dd82ad16b
rll2641 <fair0838@gmail.com>
2022-06-05 19:07:38 +0900

    다익스트라를 이용해 최단거리 알아내기

--- Python_Project/Subway_Station/subway_dijkstra.py
+++ Python_Project/Subway_Station/subway_dijkstra.py
@@ -3,46 +3,45 @@ import heapq
 import sys
 
 station_dgraph = { # 지하철 노선도
-    '용산': {'이촌': 2},
-    '이촌': {'용산':2, '서빙고': 2, '동작': 3},
-    '서빙고': {'이촌': 2, '한남': 2},
-    '한남': {'서빙고':2 , '옥수': 2},
-    '옥수': {'한남': 2, '압구정': 4},
-    '흑석': {'동작': 1},
-    '동작': {'흑석': 1, '구반포': 1, '총신대입구': 2, '이촌': 3},
-    '구반포': {'동작': 1, '신반포': 1},
-    '신반포': {'구반포':1, '고속터미널': 1},
-    '고속터미널': {'신반포': 1, '잠원': 1, '교대': 2, '내방': 2},
-    '압구정': {'옥수': 4, '신사': 2},
-    '신사': {'압구정':2, '잠원': 1},
-    '잠원': {'신사': 1, '고속터미널': 1},
-    '총신대입구': {'동작': 2, '내방': 1, '사당': 1},
-    '내방': {'총신대입구': 1, '고속터미널': 2},
-    '사당': {'총신대입구': 1, '방배': 2},
-    '방배': {'사당': 2, '서초': 2},
-    '서초': {'방배':2, '교대': 1},
-    '교대': {'서초': 1, '고속터미널': 2}
+    '용산': {'이촌': 1.9},
+    '이촌': {'용산': 1.9, '서빙고': 1.7, '동작': 2.7},
+    '서빙고': {'이촌': 1.7, '한남': 1.9},
+    '한남': {'서빙고': 1.9 , '옥수': 1.6},
+    '옥수': {'한남': 1.6, '압구정': 2.1},
+    '흑석': {'동작': 1.4},
+    '동작': {'흑석': 1.4, '구반포': 1.0, '총신대입구': 1.8, '이촌': 2.7},
+    '구반포': {'동작': 1.0, '신반포': 1.0},
+    '신반포': {'구반포': 1.0, '고속터미널': 1.0},
+    '고속터미널': {'신반포': 1.0, '잠원': 1.2, '교대': 1.6, '내방': 2.2},
+    '압구정': {'옥수': 2.1, '신사': 1.5},
+    '신사': {'압구정': 1.5, '잠원': 1.0},
+    '잠원': {'신사': 1.0, '고속터미널': 1.2},
+    '총신대입구': {'동작': 1.8, '내방': 1.0, '사당': 1.1},
+    '내방': {'총신대입구': 1.0, '고속터미널': 2.2},
+    '사당': {'총신대입구': 1.1, '방배': 1.6},
+    '방배': {'사당': 1.6, '서초': 1.7},
+    '서초': {'방배': 1.7, '교대': 1.0},
+    '교대': {'서초': 1.0, '고속터미널': 1.6}
 }
 
 def dijkstra(start):
 
-    distances = {node: sys.maxsize for node in station_dgraph}
-    distances[start] = 0
+    station_distance = {node: sys.maxsize for node in station_dgraph}
+    station_distance[start] = 0
     que = []
-    
-    heapq.heappush(que, (distances[start], start))
-    
+    heapq.heappush(que, (station_distance[start], start))
+
     while que:
-        current_distance, node = heapq.heappop(que)
-    
-        if distances[node] < current_distance:
+      distance, node = heapq.heappop(que)
+      
+      if station_distance[node] < distance:
             continue
+          
+      for n, d in station_dgraph[node].items():
+        min_distance = distance + d
         
-        for adjacency_node, distance in station_dgraph[node].items():
-            weighted_distance = current_distance + distance
-            
-            if weighted_distance < distances[adjacency_node]:
-                distances[adjacency_node] = weighted_distance
-                heapq.heappush(que, (weighted_distance, adjacency_node))
-    
-    return distances
+        if min_distance < station_distance[n]:
+          station_distance[n] = min_distance
+          heapq.heappush(que,(min_distance, n))
+
+    return station_distance
